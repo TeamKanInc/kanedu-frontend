a71e9c4b555eaf0345551a0bb6a23804aeea7573
BeginerLord <133072912+BeginerLord@users.noreply.github.com>
2024-08-05 23:10:59 -0500

    added screen not found

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Login from "./Screens/Auth/Login";
 import Signup from "./Screens/Auth/Signup";
 import GuardRoutes from "./guard/ProtectedRoute";
 import Private from "./Screens/Private";
+import NotFound from "./Screens/NotFound";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path={"/login"} element={<Login />} />
+          <Route path={"/notfound"} element={<NotFound/>} />
           <Route path={"/registrarse"} element={<Signup />} />
           <Route element={<GuardRoutes isAuth={true} />}>
             <Route path={"*"} element={<Private />} />
--- src/Screens/index.ts
+++ src/Screens/index.ts
@@ -1,2 +1,5 @@
-export * from './Private';
-export * from './Auth';
+export * from './Auth/Login/index';
+export * from './Auth/Signup/index';
+export * from './NotFound/index';
+export * from './Private/index';
+export * from './Private/Workzone/index';
