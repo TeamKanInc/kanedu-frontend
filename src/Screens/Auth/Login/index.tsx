3024c880f754bfb1e69f637477470ea3e0c2f2f9
BeginerLord <133072912+BeginerLord@users.noreply.github.com>
2024-08-06 18:35:46 -0500

    added isLoader

--- src/Screens/Auth/Login/index.tsx
+++ src/Screens/Auth/Login/index.tsx
@@ -5,6 +5,7 @@ import { ButtonComponent, InputComponent } from "../../../components";
 import LinkButton from "../../../components/ui/LinkButton";
 import { useLoginGoogle } from "../../../hooks/auth/useLoginGoogle";
 import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import { IsLoaderComponent } from "../../../components/ui/IsLoader";
 
 const Login = () => {
   const { isPending, login } = useLogin();
@@ -29,41 +30,44 @@ const Login = () => {
   const handleGoogleError = () => {};
 
   return (
-    <div className={styles.container}>
-      <div className={styles.form}>
-        <div className={styles.img_logo_container}>
-          <img className={styles.img_logo} src="/img/logo.png" alt="logo" />
-        </div>
-        <div className={styles.header_form}>
-          <h2>Bienvenido</h2>
-          <span>Encantado de verte nuevamente, ¡inicia sesion!</span>
-        </div>
-        <div className={styles.social}>
-          <GoogleLogin
-            useOneTap
-            onError={handleGoogleError}
-            onSuccess={handleGoogleSuccess}
+    <>
+      {isPending ? <IsLoaderComponent /> : null}
+      <div className={styles.container}>
+        <div className={styles.form}>
+          <div className={styles.img_logo_container}>
+            <img className={styles.img_logo} src="/img/logo.png" alt="logo" />
+          </div>
+          <div className={styles.header_form}>
+            <h2>Bienvenido</h2>
+            <span>Encantado de verte nuevamente, ¡inicia sesión!</span>
+          </div>
+          <div className={styles.social}>
+            <GoogleLogin
+              useOneTap
+              onError={handleGoogleError}
+              onSuccess={handleGoogleSuccess}
+            />
+          </div>
+          <div className={styles.container_input}>
+            <InputComponent setInfo={setEmail} label="Correo" type="email" />
+            <InputComponent
+              setInfo={setPassword}
+              label="Contraseña"
+              type="password"
+            />
+            <ButtonComponent
+              onClick={handleLogin}
+              label={isPending ? "Cargando..." : "Acceder"}
+            />
+          </div>
+          <LinkButton
+            url="http://localhost:5173/registrarse"
+            label="¿No tienes una cuenta?"
+            name="Regístrate aquí"
           />
         </div>
-        <div className={styles.container_input}>
-          <InputComponent setInfo={setEmail} label="Correo" type="email" />
-          <InputComponent
-            setInfo={setPassword}
-            label="Contraseña"
-            type="password"
-          />
-          <ButtonComponent
-            onClick={handleLogin}
-            label={isPending ? "Cargando..." : "Acceder"}
-          />
-        </div>
-        <LinkButton
-          url={"http://localhost:5173/registrarse"}
-          label="¿No tienes una cuenta?"
-          name="Registrate aqui"
-        />
       </div>
-    </div>
+    </>
   );
 };
 
